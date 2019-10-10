5ced47ee083352680bb1d7f86b664c2937f0d3d8
U-DPO\gree02 <gree02@OT-19006.dpo.fprod>
2019-10-10 11:55:55 -0400

    Fetch LimeSurvey from github releases

---
+++ 000init.sh
@@ -0,0 +1,213 @@
+#!/bin/bash
+set -eu
+shopt -s extglob globstar dotglob
+
+# usage: file_env VAR [DEFAULT]
+#    ie: file_env 'XYZ_DB_PASSWORD' 'example'
+# (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
+#  "$XYZ_DB_PASSWORD" from a file, especially for Docker's secrets feature)
+file_env() {
+	local var="$1"
+	local fileVar="${var}_FILE"
+	local def="${2:-}"
+	if [ "${!var:-}" ] && [ "${!fileVar:-}" ]; then
+		echo >&2 "error: both ${var} and ${fileVar} are set (but are exclusive)"
+		exit 1
+	fi
+	local val="${def}"
+	if [ "${!var:-}" ]; then
+		val="${!var}"
+	elif [ "${!fileVar:-}" ]; then
+		val="$(< "${!fileVar}")"
+	fi
+	export "${var}"="${val}"
+	unset "${fileVar}"
+}
+
+cd "${WEB_DOCUMENT_ROOT}"
+
+if [ ! -f ".RELEASE_${LIMESURVEY_GIT_RELEASE}" ]; then
+    echo >&2 "Retrieving LimeSurvey... "
+    curl -sSL "https://github.com/LimeSurvey/LimeSurvey/archive/${LIMESURVEY_GIT_RELEASE}.tar.gz" -o "/tmp/lime.tar.gz"
+
+    echo >&2 'Extracting files from archive...'
+    tar -xzf /tmp/lime.tar.gz \
+        --group=application \
+        --owner=application \
+        --strip-components=1 \
+        --keep-newer-files \
+        --exclude-vcs \
+        --to-command="sh -c $(printf '%q' 'mkdir -p $(dirname "./$TAR_FILENAME") && touch "./$TAR_FILENAME" && dd of="./$TAR_FILENAME" >/dev/null 2>&1 && echo "./$TAR_FILENAME" ')" | \
+        xargs -I {} touch -t 195001010000 {}
+    
+    rm /tmp/lime.tar.gz
+fi
+
+file_env 'LIMESURVEY_DB_TYPE' 'mysql'
+file_env 'LIMESURVEY_DB_HOST' 'mysql'
+file_env 'LIMESURVEY_DB_PORT' '3306'
+file_env 'LIMESURVEY_TABLE_PREFIX' ''
+file_env 'LIMESURVEY_ADMIN_NAME' 'Lime Administrator'
+file_env 'LIMESURVEY_ADMIN_EMAIL' 'lime@lime.lime'
+file_env 'LIMESURVEY_ADMIN_USER' ''
+file_env 'LIMESURVEY_ADMIN_PASSWORD' ''
+file_env 'LIMESURVEY_DEBUG' '0'
+file_env 'LIMESURVEY_SQL_DEBUG' '0'
+file_env 'MYSQL_SSL_CA' ''
+file_env 'LIMESURVEY_USE_INNODB' ''
+
+# if we're linked to MySQL and thus have credentials already, let's use them
+file_env 'LIMESURVEY_DB_NAME' "${MYSQL_ENV_MYSQL_DATABASE:-limesurvey}"
+file_env 'LIMESURVEY_DB_USER' "${MYSQL_ENV_MYSQL_USER:-root}"
+
+if [ "${LIMESURVEY_DB_USER}" = 'root' ]; then
+    file_env 'LIMESURVEY_DB_PASSWORD' "${MYSQL_ENV_MYSQL_ROOT_PASSWORD:-}"
+else
+    file_env 'LIMESURVEY_DB_PASSWORD' "${MYSQL_ENV_MYSQL_PASSWORD:-}"
+fi
+
+if [ -z "${LIMESURVEY_DB_PASSWORD}" ]; then
+    echo >&2 'error: missing required LIMESURVEY_DB_PASSWORD environment variable'
+    echo >&2 '  Did you forget to -e LIMESURVEY_DB_PASSWORD=... ?'
+    echo >&2
+    echo >&2 '  (Also of interest might be LIMESURVEY_DB_USER and LIMESURVEY_DB_NAME.)'
+    exit 1
+fi
+
+declare -A CONNECTION_STRINGS=(
+    [mysql]="mysql:host=${LIMESURVEY_DB_HOST};port=${LIMESURVEY_DB_PORT};dbname=${LIMESURVEY_DB_NAME};"
+    [dblib]="dblib:host=${LIMESURVEY_DB_HOST};dbname=${LIMESURVEY_DB_NAME}"
+    [pgsql]="pgsql:host=${LIMESURVEY_DB_HOST};port=${LIMESURVEY_DB_PORT};user=${LIMESURVEY_DB_USER};password=${LIMESURVEY_DB_PASSWORD};dbname=${LIMESURVEY_DB_NAME};"
+    [sqlsrv]="sqlsrv:Server=${LIMESURVEY_DB_HOST};Database=${LIMESURVEY_DB_NAME}"
+)
+
+if [ -z ${CONNECTION_STRINGS[${LIMESURVEY_DB_TYPE}]} ]; then
+    echo >&2 "error: invalid database type: ${LIMESURVEY_DB_TYPE}"
+    echo >&2 "  LIMESURVEY_DB_TYPE must be either \"mysql\", \"dblib\", \"pgsql\" or \"sqlsrv\"."
+    exit 1
+fi
+
+if ! [ -e application/config/config.php ]; then
+    echo >&2 "No config file in $(pwd) Copying default config file..."
+    #Copy default config file but also allow for the addition of attributes
+    echo "'attributes' => array()," | awk '/lime_/ && c == 0 { c = 1; system("cat") } { print }' application/config/config-sample-mysql.php > application/config/config.php
+fi
+
+# Install BaltimoreCyberTrustRoot.crt.pem
+if [ ! -e BaltimoreCyberTrustRoot.crt.pem ]; then
+    echo "Downloading BaltimoreCyberTrustroot.crt.pem"
+    curl -o BaltimoreCyberTrustRoot.crt.pem -fsL "https://www.digicert.com/CACerts/BaltimoreCyberTrustRoot.crt.pem"
+fi
+
+# see http://stackoverflow.com/a/2705678/433558
+sed_escape_lhs() {
+    echo "$@" | sed -e 's/[]\/$*.^|[]/\\&/g'
+}
+sed_escape_rhs() {
+    echo "$@" | sed -e 's/[\/&]/\\&/g'
+}
+php_escape() {
+    php -r 'var_export(('$2') $argv[1]);' -- "$1"
+}
+set_config() {
+    key="$1"
+    value="$2"
+    sed -i "/'$key'/s/>\(.*\)/>$value,/1"  application/config/config.php
+}
+
+set_config 'connectionString' "'${CONNECTION_STRINGS[${LIMESURVEY_DB_TYPE}]}'"
+set_config 'tablePrefix' "'${LIMESURVEY_TABLE_PREFIX}'"
+set_config 'username' "'${LIMESURVEY_DB_USER}'"
+set_config 'password' "'${LIMESURVEY_DB_PASSWORD}'"
+set_config 'urlFormat' "'path'"
+set_config 'debug' "${LIMESURVEY_DEBUG}"
+set_config 'debugsql' "${LIMESURVEY_SQL_DEBUG}"
+
+if [ -n "${MYSQL_SSL_CA}" ]; then
+    set_config 'attributes' "array(PDO::MYSQL_ATTR_SSL_CA => '\/var\/www\/html\/${MYSQL_SSL_CA}', PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false)"
+fi
+
+if [ -n "${LIMESURVEY_USE_INNODB}" ]; then
+    #If you want to use INNODB - remove MyISAM specification from LimeSurvey code
+    sed -i "/ENGINE=MyISAM/s/\(ENGINE=MyISAM \)//1" application/core/db/MysqlSchema.php
+fi
+
+chown application:application -R tmp 
+mkdir -p upload/surveys
+chown application:application -R upload 
+chown application:application -R application/config
+
+echo "Waiting for database..."
+while ! curl -sL "${LIMESURVEY_DB_HOST}:${LIMESURVEY_DB_PORT:-3306}" 2>/dev/null; do sleep 1; done
+
+DBSTATUS=$(TERM=dumb php -- "${LIMESURVEY_DB_HOST}" "${LIMESURVEY_DB_USER}" "${LIMESURVEY_DB_PASSWORD}" "${LIMESURVEY_DB_NAME}" "${LIMESURVEY_TABLE_PREFIX}" "${MYSQL_SSL_CA}" <<'EOPHP'
+<?php
+// database might not exist, so let's try creating it (just to be safe)
+
+error_reporting(E_ERROR | E_PARSE);
+
+$stderr = fopen('php://stderr', 'w');
+
+list($host, $socket) = explode(':', $argv[1], 2);
+$port = 0;
+if (is_numeric($socket)) {
+        $port = (int) $socket;
+        $socket = null;
+}
+
+$maxTries = 10;
+do {
+    $con = mysqli_init();
+    if (isset($argv[6]) && !empty($argv[6])) {
+        mysqli_ssl_set($con,NULL,NULL,"/var/www/html/" . $argv[6],NULL,NULL);
+    }
+    $mysql = mysqli_real_connect($con,$host, $argv[2], $argv[3], '', $port, $socket, MYSQLI_CLIENT_SSL_DONT_VERIFY_SERVER_CERT);
+        if (!$mysql) {
+                fwrite($stderr, "\n" . 'MySQL Connection Error: (' . $mysql->connect_errno . ') ' . $mysql->connect_error . "\n");
+                --$maxTries;
+                if ($maxTries <= 0) {
+                        exit(1);
+                }
+                sleep(3);
+        }
+} while (!$mysql);
+
+if (!$con->query('CREATE DATABASE IF NOT EXISTS `' . $con->real_escape_string($argv[4]) . '`')) {
+        fwrite($stderr, "\n" . 'MySQL "CREATE DATABASE" Error: ' . $con->error . "\n");
+        $con->close();
+        exit(1);
+}
+
+$con->select_db($con->real_escape_string($argv[4]));
+
+$inst = $con->query("SELECT * FROM `" . $con->real_escape_string($argv[5]) . "users" . "`");
+
+$con->close();
+
+if ($inst->num_rows > 0) {
+        exit("DBEXISTS");
+} else {
+        exit(0);
+}
+
+EOPHP
+) >/dev/null
+
+# cd application/commands/
+if [ "${DBSTATUS}" != "DBEXISTS" ] &&  [ -n "${LIMESURVEY_ADMIN_USER}" ] && [ -n "${LIMESURVEY_ADMIN_PASSWORD}" ]; then
+    echo >&2 'Database not yet populated - installing Limesurvey database'
+    su - application -c php ./application/commands/console.php install "${LIMESURVEY_ADMIN_USER}" "${LIMESURVEY_ADMIN_PASSWORD}" "${LIMESURVEY_ADMIN_NAME}" "${LIMESURVEY_ADMIN_EMAIL}" verbose
+fi
+
+if [ -f './application/commands/UpdateDbCommand.php' ]; then
+    echo >&2 'Updating database'
+    su - application -c php ./application/commands/console.php updatedb
+else
+    echo >&2 'WARNING: Manual database update may be required!'
+fi
+
+if [ -n "${LIMESURVEY_ADMIN_USER}" ] && [ -n "${LIMESURVEY_ADMIN_PASSWORD}" ]; then
+    echo >&2 'Updating password for admin user'
+    su - application -c php ./application/commands/console.php resetpassword "${LIMESURVEY_ADMIN_USER}" "${LIMESURVEY_ADMIN_PASSWORD}"
+fi
+
--- Dockerfile
+++ Dockerfile
@@ -1,56 +1,15 @@
-FROM php:7.2-apache
+FROM webdevops/php-apache
 
-ENV DOWNLOAD_URL https://www.limesurvey.org/stable-release?download=2657:limesurvey3180%20190923targz
-ENV DOWNLOAD_SHA256 55394ea29878d8f7d73ecc8ab0fa8719dc95007c9ab5b3d3c024d1a095b004bd
+ENV LIMESURVEY_GIT_RELEASE 2.05_plus_141123
 
-# install the PHP extensions we need
-RUN apt-get update && apt-get install -y libc-client-dev libfreetype6-dev libmcrypt-dev libpng-dev libjpeg-dev libldap2-dev zlib1g-dev libkrb5-dev libtidy-dev libzip-dev libsodium-dev && rm -rf /var/lib/apt/lists/* \
-	&& docker-php-ext-configure gd --with-freetype-dir=/usr/include/  --with-png-dir=/usr --with-jpeg-dir=/usr \
-	&& docker-php-ext-install gd mysqli pdo pdo_mysql opcache zip iconv tidy \
-    && docker-php-ext-configure ldap --with-libdir=lib/x86_64-linux-gnu/ \ 
-    && docker-php-ext-install ldap \ 
-    && docker-php-ext-configure imap --with-imap-ssl --with-kerberos \ 
-    && docker-php-ext-install imap \
-    && docker-php-ext-install sodium \
-    && pecl install mcrypt-1.0.1 \
-    && docker-php-ext-enable mcrypt
+RUN phpenmod imap 
 
 RUN a2enmod rewrite
 
-# set recommended PHP.ini settings
-# see https://secure.php.net/manual/en/opcache.installation.php
-RUN { \
-		echo 'opcache.memory_consumption=128'; \
-		echo 'opcache.interned_strings_buffer=8'; \
-		echo 'opcache.max_accelerated_files=4000'; \
-		echo 'opcache.revalidate_freq=2'; \
-		echo 'opcache.fast_shutdown=1'; \
-		echo 'opcache.enable_cli=1'; \
-	} > /usr/local/etc/php/conf.d/opcache-recommended.ini
+COPY 000init.sh /entrypoint.d/000init.sh
+RUN chmod +x /entrypoint.d/000init.sh
 
-RUN set -x; \
-	curl -SL "$DOWNLOAD_URL" -o /tmp/lime.tar.gz; \
-    echo "$DOWNLOAD_SHA256 /tmp/lime.tar.gz" | sha256sum -c -; \
-    tar xf /tmp/lime.tar.gz --strip-components=1 -C /var/www/html; \
-    rm /tmp/lime.tar.gz; \
-    chown -R www-data:www-data /var/www/html
-
-#Set PHP defaults for Limesurvey (allow bigger uploads)
-RUN { \
-		echo 'memory_limit=256M'; \
-		echo 'upload_max_filesize=128M'; \
-		echo 'post_max_size=128M'; \
-		echo 'max_execution_time=120'; \
-        echo 'max_input_vars=10000'; \
-        echo 'date.timezone=UTC'; \
-	} > /usr/local/etc/php/conf.d/uploads.ini
+SHELL [ "/bin/bash", "--login", "-c" ]
 
 VOLUME ["/var/www/html/plugins"]
 VOLUME ["/var/www/html/upload"]
-
-COPY docker-entrypoint.sh /usr/local/bin/
-RUN ln -s usr/local/bin/docker-entrypoint.sh /entrypoint.sh # backwards compat
-
-# ENTRYPOINT resets CMD
-ENTRYPOINT ["docker-entrypoint.sh"]
-CMD ["apache2-foreground"]
